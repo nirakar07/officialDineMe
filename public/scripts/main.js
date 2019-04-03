45fc2db1d8d52a88bede988ea7b24790e71b579c
nirakar07 <44928388+nirakar07@users.noreply.github.com>
2019-04-02 20:08:57 -0400

    Update main.js

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,3 +1,23 @@
 $('#datetimepicker1').on('click', function(){
     $('#datetime').val($(this).text());
 });
+
+$('#demolist1 li').on('click', function(){
+    $('#datebox1').val($(this).text());
+});
+
+$('#demolist2 li').on('click', function(){
+    $('#datebox2').val($(this).text());
+});
+
+$('#demolist3 li').on('click', function(){
+    $('#datebox3').val($(this).text());
+});
+
+$('#demolist4 li').on('click', function(){
+    $('#datebox4').val($(this).text());
+});
+
+$('#calendar span').on('click', function(){
+    $('#datetime').val($(this).text());
+});
